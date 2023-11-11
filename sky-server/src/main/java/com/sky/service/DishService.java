7e6d88dc99a60d20401e2c15550326882c20f241
wangqi <1325041344@qq.com>
2023-11-11 22:57:23 +0800

    商品浏览代码业务开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -30,4 +30,10 @@ public interface DishService {
     void updateWithFlavor(DishDTO dishDTO);
 
     List<Dish> listdishes(Long categoryId);
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
