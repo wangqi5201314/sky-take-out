602e2cc9a4713b4c252a5baa6ba78898b371e8c1
wangqi <1325041344@qq.com>
2023-11-12 09:35:26 +0800

    菜品缓存业务开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -22,11 +24,16 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping
     @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品信息{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        //精确清理缓存
+        redisTemplate.delete("dish_"+dishDTO.getCategoryId());
+
         return Result.success();
     }
 
@@ -50,6 +57,7 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("菜品批量删除：{}", ids);
         dishService.deleteBatch(ids);//后绪步骤实现
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -71,6 +79,7 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -84,5 +93,14 @@ public class DishController {
         log.info("分类id为,,{}",categoryId);
         List<Dish> listdishes = dishService.listdishes(categoryId);
         return Result.success(listdishes);
+    }
+
+    /**
+     * 清理全部缓存
+     * @param pattern
+     */
+    public void cleanCache(String pattern) {
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 }
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,9 +9,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController("userDishController")
@@ -21,6 +23,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 根据分类id查询菜品
@@ -31,12 +35,25 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        //构造一个key
+        String key = "dish_" + categoryId;
+        //查询redis,存在key则返回
+        List<DishVO> value =
+                (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (value!=null&&value.size()>0) {
+            //存在,即返回
+            return Result.success(value);
+        }
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
 
+
         List<DishVO> list = dishService.listWithFlavor(dish);
 
+        //不存在,查询数据库后将查询到的数据添加到redis
+        redisTemplate.opsForValue().set(key,list);
         return Result.success(list);
     }
 
