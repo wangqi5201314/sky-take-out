4929f79d15d3de0a14fd23460c0a9881ac6dea68
wangqi <1325041344@qq.com>
2023-11-13 14:26:21 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -10,4 +14,21 @@ public interface OrderMapper {
      * @param order
      */
     void insert(Orders order);
+    /**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    @Select("select * from orders where status=#{pendingPayment} " +
+            "and order_time <#{time}")
+    List<Orders> getByStatusAndOrderTime(Integer pendingPayment, LocalDateTime time);
 }
